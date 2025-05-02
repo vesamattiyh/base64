a1fc089db771db95f16f17d42ef0466c776be1ac
vesamattiyh <97747456+vesamattiyh@users.noreply.github.com>
2025-05-02 14:24:37 +0300

    Update base64.h

--- base64.h
+++ base64.h
@@ -6,7 +6,6 @@
 #include <stdint.h>
 #include <string.h>
 
-// unsigned char* base64_encode(char* input_text, int length);
 unsigned char *base64_encode(const unsigned char *src, size_t len);
 unsigned char *base64_decode(const unsigned char *src, size_t len);
 
